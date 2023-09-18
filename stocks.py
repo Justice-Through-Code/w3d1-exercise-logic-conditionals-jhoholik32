6ac1c7976801593741bed95201f4d48b5344bb48
James L Hoholik <jhoholik32@gmail.com>
2023-09-17 20:01:31 -0700

    done

--- stocks.py
+++ stocks.py
@@ -20,7 +20,7 @@ def stock_purchases():
     
     # Use `if/elif/else` conditional logic to determine how much stock the client can buy,
     # and save it in a variable
-    shares = apple / 2
+    shares = stock_name 
     if stock_name == "amazon":
         shares = invest_amount / amazon
     elif stock_name == "apple":
