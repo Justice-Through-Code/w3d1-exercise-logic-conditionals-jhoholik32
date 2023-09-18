253d130194a3ac581cb95f617d25501f2a17394d
James L Hoholik <jhoholik32@gmail.com>
2023-09-17 19:55:52 -0700

    done

--- snippets.py
+++ snippets.py
@@ -60,6 +60,6 @@ def snippet_5():
 
     o = (m != n)
 
-    print (f"The value of o ({o}) is False since Python is case-sensitive.")
+    print (f"The value of o ({o}) is True since Python is case-sensitive.")
     assert(o == True)  # <-- DO NOT EDIT THIS LINE
 snippet_5()
