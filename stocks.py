16b692ecfee580b37d0ea5f16013daaa96815a2e
James L Hoholik <jhoholik32@gmail.com>
2023-09-17 19:49:57 -0700

    done

--- stocks.py
+++ stocks.py
@@ -37,4 +37,3 @@ def stock_purchases():
     # 1.5 TODO: Once you've calculated the number of stocks that can be purchased,
     # Use an f-string to print the result for the client, ala:
     # Alex has $5000 to invest and can buy 50 shares of Apple at the current price of $100.
-stock_purchases()
