6471c92b3510ffe22b33363f150f800ec53f8fa0
James L Hoholik <jhoholik32@gmail.com>
2023-09-19 12:17:14 -0700

    done

--- stocks.py
+++ stocks.py
@@ -8,11 +8,11 @@ def stock_purchases():
 
     # Given the prices above and a client's investment budget, how much stock can they buy?
     # 1.1 TODO: Ask the client's name (use the string: "What is your name? ") and save it into a variable
-    name=input("What is your name? ")
+    name = input("What is your name? ")
     # 1.2 TODO: Ask the client how many dollars they would like to invest (use the string: "How much would you like to invest? $")
     # and save it into a variable
     # NOTE: When you use the `input` function to get user input, what do numbers get saved as?
-    invest_amount = input("How much would you like to invest? $")
+    invest_amount = int(input("How much would you like to invest? $"))
     # 1.3 TODO: Uncomment the line below to ask the client which stock they're interested in.
     # NOTE: Take a look at how this input string prints out
     stock_name = input("\nWhich stock are you interested in? Enter the full name:\nAmazon\nApple\nFacebook\nGoogle\nMicrosoft\nStock Name: ")
@@ -39,8 +39,8 @@ def stock_purchases():
         shares = invest_amount // msft
         stock_price = msft
 
-    "
+    
     # 1.5 TODO: Once you've calculated the number of stocks that can be purchased,
     # Use an f-string to print the result for the client, ala:
     # Alex has $5000 to invest and can buy 50 shares of Apple at the current price of $100.
-    print(f"{name}has{invest_amount}to invest and can buy {int(shares)}of {stock_name}  at the current price of ${stock_price}. ")
+    print(f"{name} has ${invest_amount} to invest and can buy {int(shares)} shares of {stock_name} at the current price of ${stock_price}.")
